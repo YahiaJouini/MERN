f079c0e038efeca709e50b270d8fda9852fb900d
Yahia Jouini <jouiniyahya117@gmail.com>
2023-11-17 21:32:49 +0100

    new react assignment

---
+++ React/more-forms/src/App.tsx
@@ -0,0 +1,58 @@
+import Fields from "./components/Fields"
+
+import { useState } from "react"
+function App() {
+
+  const [Fname, setFname] = useState("")
+  const [Lname, setLname] = useState("")
+  const [email, setEmail] = useState("")
+  const [pwd, setPwd] = useState("")
+  const [confirm, setConfirm] = useState("")
+
+  const style = "h-8 w-3/5 border-2 border-solid rounded-[8px] " // styling the input field
+
+  return (
+    <div className="flex flex-col items-center h-[100vh]">
+      <form>
+
+        <Fields data="First Name">
+          <input type="text" className={style} onChange={(e) => setFname(e.target.value)} />
+        </Fields>
+
+        {Fname.length<2 && <h1 className="mt-[-30px] text">First Name must be at least 2 characters</h1>}
+
+
+        <Fields data="Last Name" >
+          <input type="text" className={style} onChange={(e) => setLname(e.target.value)} />
+        </Fields>
+
+        {Lname.length<2 && <h1 className="mt-[-30px] text">Last Name must be at least 2 characters</h1>}
+
+
+        <Fields data="Email" >
+          <input type="text" className={style} onChange={(e) => setEmail(e.target.value)} />
+        </Fields>
+        
+        {email.length<2 && <h1 className="mt-[-30px] text">Email must be at least 2 characters</h1>}
+
+
+        <Fields data="Password" >
+          <input type="text" className={style} onChange={(e) => setPwd(e.target.value)} />
+        </Fields>
+
+        {pwd.length<8 && <h1 className="mt-[-30px] text">Password must be at least 8 characters</h1>}
+
+
+        <Fields data="Confirm Password" >
+          <input type="text" className={style} onChange={(e) => setConfirm(e.target.value)} />
+        </Fields>
+
+        {pwd!==confirm || (pwd===confirm && confirm==="")  ? <h1 className="mt-[-30px] text">Passwords must match</h1> : <></>}
+
+
+      </form>
+    </div>
+  )
+}
+
+export default App
